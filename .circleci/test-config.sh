53798c25334dad2302bfb643720d8ceecedf34ef
Sagar Patke <sagarpatke@gmail.com>
2018-08-23 06:03:17 +0530

    Changing to v1.0 compatibility

--- .circleci/test-config.sh
+++ .circleci/test-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=807bab50b907da67907932ba3a8beced7341b457 \
+    --form revision=d988cf98d83a53790c3cfa2171e44d6046ef4473 \
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/sagarpatke/ci-cd-test/tree/master
